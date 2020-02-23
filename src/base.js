e4c9d94a05054edb4c4053773976cdf53c466781
renan <renanalves23@outlook.com>
2020-02-22 23:16:28 -0300

    base.js config. E yarn add re-base

---
+++ src/base.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBicL29qCy1zS9tsZvnFOY9cc0HCxxGVxU",
+  authDomain: "mercadodev-8646c.firebaseapp.com",
+  databaseURL: "https://mercadodev-8646c.firebaseio.com",
+  projectId: "mercadodev-8646c",
+  storageBucket: "mercadodev-8646c.appspot.com",
+  messagingSenderId: "1084894545160",
+  appId: "1:1084894545160:web:58f2af0d29066e15a718bd",
+  measurementId: "G-QCPRHGCSBZ"
+};
