8f62e9f3160e8dde8c27c76fa28704198e8e0529
renan <renanalves23@outlook.com>
2020-02-23 10:09:47 -0300

    anuncios

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ class App extends Component {
       super(props)
 
         this.state = {
-            categorias: []
+            categorias: [],
+            //anuncios
           }
           base.bindToState('categorias', {
             context: this,
