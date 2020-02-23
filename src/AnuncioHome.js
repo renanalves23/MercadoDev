0d63991820f89b7011cfd5c973fa5a734bb1b46f
renan <renanalves23@outlook.com>
2020-02-23 10:24:17 -0300

    descricao, nome, preco e foto

--- src/AnuncioHome.js
+++ src/AnuncioHome.js
@@ -4,13 +4,13 @@ const AnuncioHome = ({anuncio}) => {
   return (
       <div className="col-lg-4 col-md-6 mb-4">
           <div className="card h-100">
-              <a href="#"><img className="card-img-top" src="http://placehold.it/200x140" alt="" /></a>
+              <a href="#"><img className="card-img-top" src={anuncio.foto} alt="Foto" /></a>
               <div className="card-body">
                 <h4 className="card-title">
                   <a to={`/anuncios/ver/`}>{anuncio.nome}</a>
                 </h4>
-                <h5>preco</h5>
-              <p className="card-text">descricao</p>
+                <h5>{anuncio.preco}</h5>
+              <p className="card-text">{anuncio.descricao}</p>
             </div>
           </div>
     </div>
